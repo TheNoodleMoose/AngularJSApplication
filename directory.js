683dd44e2e082a45c1baa6f19be27e6339bdceab
Christian Huffman <cjh605@gmail.com>
2019-03-11 11:52:25 -0400

    Added a view for our persons

--- directory.js
+++ directory.js
@@ -4,10 +4,29 @@ angular
     const dirList = this;
 
     dirList.list = [
-      { name: "Christian", age: 20 },
-      { name: "Kaylee", age: 19 },
-      { name: "Caleb", age: 10 },
-      { name: "Greg", age: 48 }
+      {
+        name: "Christian",
+        age: 20,
+        img:
+          "https://pbs.twimg.com/profile_images/1055263632861343745/vIqzOHXj.jpg"
+      },
+      {
+        name: "Kaylee",
+        age: 19,
+        img:
+          "https://tinyfac.es/data/avatars/A7299C8E-CEFC-47D9-939A-3C8CA0EA4D13-200w.jpeg"
+      },
+      {
+        name: "Caleb",
+        age: 10,
+        img:
+          "https://tinyfac.es/data/avatars/7D3FA6C0-83C8-4834-B432-6C65ED4FD4C3-500w.jpeg"
+      },
+      {
+        name: "Greg",
+        age: 48,
+        img: "https://randomuser.me/api/portraits/men/36.jpg"
+      }
     ];
 
     dirList.addPerson = () => {
